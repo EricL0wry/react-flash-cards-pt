85aa1ec221e718a083bbb25659d0a321c6aad1e0
Eric Lowry <ericmichaellowry@gmail.com>
2020-04-20 17:00:05 -0700

    Initial project setups completed

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      butts: 'butts'
+    };
+
+  }
+
+  render() {
+    return (
+      <h1 className="text-center">Flash Card App</h1>
+    );
+  }
+}
+
+export default App;
